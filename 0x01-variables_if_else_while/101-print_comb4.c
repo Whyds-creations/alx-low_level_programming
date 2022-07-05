a64715d73dac9fda3673b07466188c296fb074e8
Popoola Taoheed <whydscreations@gmail.com>
2022-07-04 20:31:56 -0700

    alx task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 	int i, j, k;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
 			for (k = '0'; k <= '9'; k++)
-			{ 
+			{
 				if ((i < j) & (j < k) & (k <= '9'))
 				{
 					putchar(i);
